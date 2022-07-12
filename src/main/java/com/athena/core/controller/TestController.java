6bce5243340814fd5d392da82ebc6f30ea35d799
tejas <tejas.nevage@thinkitive.com>
2022-07-12 15:37:56 +0530

    athena-module migrtion code for review

--- src/main/java/com/athena/core/controller/TestController.java
+++ src/main/java/com/athena/core/controller/TestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+
     @Autowired
     private EHRAthenaConfig ehrAthenaConfig;
 
